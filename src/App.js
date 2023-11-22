9c2979c4fc58c93e2b0547499414376e6fc95755
main <ismoilergashovich@gmail.com>
2023-11-22 22:34:04 +0500

    All lessons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class State extends React.Component{
+    render(){
+      let count = 0
+      const plus = () => {
+        count = count + 1
+        console.log(count);
+      }
+      const minus = () => {
+        count = count - 1
+        console.log(count);
+      }
+
+      return(
+        <div>
+          <h1>State {count}</h1>
+          
+        </div>
+      )
+    }
 }
 
-export default App;
+export default State;
---
+++ src/review.js
@@ -0,0 +1,25 @@
+//  1-dars
+// npx create react app
+// package.json, src, public
+// single page application
+// virtual ReactDOM
+// jsx, import
+
+// ========================
+// ========================
+
+// 2-dars
+// props, components
+// export, import,
+// children
+
+
+// ========================
+// ========================
+
+// 3-dars
+
+
+// ========================
+// ========================
+// 20-dars
