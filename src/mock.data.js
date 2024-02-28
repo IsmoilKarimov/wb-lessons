a93f9576ec8fea6d27e9a6080bf288cfb11c6b86
main <ismoilergashovich@gmail.com>
2024-02-29 03:53:49 +0500

    crud

--- src/mock.data.js
+++ src/mock.data.js
@@ -1,11 +1,11 @@
 
 export const students = [
-    {id:1, name: 'Ismoil', surname: 'Karimov', job: 'IT'},
-    {id:2, name: 'Abdurahmon', surname: 'Salimov', job: 'UI'},
-    {id:3, name: 'Usmon', surname: 'Jamilov', job: 'UX'},
-    {id:4, name: 'Umar', surname: 'Azizov', job: 'BA'},
-    {id:5, name: 'Aziz', surname: 'Samandarov', job: 'CS'},
-    {id:6, name: 'Jasur', surname: 'Mahkamov', job: 'AI'},
-    {id:7, name: 'Salim', surname: 'Lazizov', job: 'AI'},
-    {id:8, name: 'Alisher', surname: 'Arziyev', job: 'IT'},
+    {id:1, name: 'Ismoil', status: 'IT'},
+    {id:2, name: 'Abdurahmon', status: 'UI'},
+    {id:3, name: 'Usmon', status: 'UX'},
+    {id:4, name: 'Umar', status: 'BA'},
+    {id:5, name: 'Aziz', status: 'CS'},
+    {id:6, name: 'Jasur', status: 'AI'},
+    {id:7, name: 'Salim', status: 'AI'},
+    {id:8, name: 'Alisher', status: 'IT'},
 ]
